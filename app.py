943fe7fca18527090fa9a49978310c9b4a1ed311
Tarkiin <61888270+Tarkiin@users.noreply.github.com>
2023-09-23 01:48:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+from lxml import html
 from colorama import Fore, init
 
 # Inicializar colorama para que funcione correctamente en todas las plataformas
@@ -11,17 +12,22 @@ def is_valid_group_link_updated(url):
     }
 
     response = requests.get(url, headers=headers)
+    content = response.text
 
     # Si la URL de la imagen genérica está presente en la respuesta, el enlace es inválido
     generic_img_url = "https://static.whatsapp.net/rsrc.php/v3/yB/r/_0dVljceIA5.png"
-    if generic_img_url in response.text:
-        return False
-    return True
+    if generic_img_url in content:
+        return False, content
+    return True, content
+
+
+def get_group_name_from_content(content):
+    tree = html.fromstring(content)
+    group_name = tree.xpath('/html/body/div[1]/div[1]/div[2]/div/section/div/div/div/div/div[2]/h3/text()')
+    return group_name[0] if group_name else "Nombre desconocido"
 
 
 def process_and_sort_links_from_file(file_path):
-    print("Iniciando el procesamiento de enlaces...")  # Mensaje de depuración
-
     # Leer el archivo y obtener todos los enlaces
     with open(file_path, "r") as file:
         links = file.readlines()
@@ -34,21 +40,45 @@ def process_and_sort_links_from_file(file_path):
     valid_links = []
     invalid_links = []
 
+    print("Iniciando el procesamiento de enlaces...")  # Mensaje de depuración
+
+    total_links = len(links)
     # Procesar cada enlace y agregarlo a la lista correspondiente
-    for link in links:
+    for index, link in enumerate(links, start=1):
         link = link.strip()  # Eliminar espacios y saltos de línea
-        if is_valid_group_link_updated(link):
-            valid_links.append(link)
+        is_valid, content = is_valid_group_link_updated(link)
+        group_name = get_group_name_from_content(content)
+
+        link_info = f"{link} - {group_name} - Válido"
+        if is_valid:
+            valid_links.append(link_info)
         else:
-            invalid_links.append(link)
+            link_info = f"{link} - {group_name} - Inválido"
+            invalid_links.append(link_info)
+
+        # Mostrar el progreso
+        print(f"\rProcesando enlaces... {index}/{total_links}", end='')
+
+    print("\nFinalizado el procesamiento de enlaces.")
+
+    # Leer los enlaces válidos previamente guardados
+    with open("links_validos.txt", "r", encoding="utf-8") as infile:
+        existing_valid_links = [line.strip() for line in infile.readlines()]
+
+    # Combinar los enlaces válidos previos con los nuevos y eliminar duplicados
+    all_valid_links = list(set(existing_valid_links + valid_links))
+
+    # Guardar los enlaces válidos en el archivo sin duplicados
+    with open("links_validos.txt", "w", encoding="utf-8") as outfile:
+        for link_info in all_valid_links:
+            outfile.write(link_info + "\n")
 
     # Imprimir los enlaces válidos
-    for link in valid_links:
-        print(Fore.GREEN + link + " - Válido")
+    for link_info in valid_links:
+        print(Fore.GREEN + link_info)
 
     # Imprimir los enlaces inválidos
-    for link in invalid_links:
-        print(Fore.RED + link + " - Inválido")
-
+    for link_info in invalid_links:
+        print(Fore.RED + link_info)
 
 process_and_sort_links_from_file("links.txt")
